69d66a2168871c951c85b85c90eb3db646a2ab61
nergal-perm <nergal@psu.ru>
2015-04-13 16:47:13 +0600

    Application web-page stub, added PrimeFaces UI

---
+++ src/main/java/ru/terekhov/book2read/control/ResourcesProvider.java
@@ -0,0 +1,14 @@
+package ru.terekhov.book2read.control;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.jboss.logging.Logger;
+
+public class ResourcesProvider {
+	
+	@Produces
+	public Logger produceLogger(InjectionPoint injectionPoint) {
+		return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+	}
+}
